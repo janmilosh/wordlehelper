2fdc4a73770140e72c35ea7d26f33054e902d663
Jan Milosh <janmilosh@gmail.com>
2022-08-04 08:27:10 -0400

    Start building grid

--- js/app.js
+++ js/app.js
@@ -3,11 +3,26 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        guesses: [["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""],
-                  ["", "", "", "", ""], ["", "", "", "", ""], ["", "", "", "", ""]],
-        message: words[200]
+        letters: []          
       }
     },
-    mounted() {   
+    mounted() { 
+      this.buildDataArray()
+    },
+    methods: {
+      buildDataArray() {
+        for (let i = 0; i < 30; i++) {
+          this.letters[i] = {text: "test" + i, state: "empty"};
+        }
+      },
+      toggleState(index) {
+        let states = ["empty", "absent", "present", "correct"];
+        switch(this.letters[index].state) {
+          case "empty": this.letters[index].state = "absent"; break;
+          case "absent": this.letters[index].state = "present"; break;
+          case "present": this.letters[index].state = "correct"; break;
+          case "correct": this.letters[index].state = "empty";
+        }
+      }
     }
   }).mount('#app')
