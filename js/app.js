71fce62d5bb4e09e5c531335b27f77d0dbe9d2c1
Jan Milosh <janmilosh@gmail.com>
2022-08-05 16:28:31 -0400

    Working grid with letter placement

--- js/app.js
+++ js/app.js
@@ -3,26 +3,60 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        letters: []          
+        letters: [],
+        indexCounter: 0
       }
     },
-    mounted() { 
-      this.buildDataArray()
+    mounted() {
+      let self = this;
+
+      this.buildDataArray();
+
+      window.addEventListener('keydown', function($event) {
+        self.handleInput($event);
+      });
     },
     methods: {
       buildDataArray() {
         for (let i = 0; i < 30; i++) {
-          this.letters[i] = {text: "test" + i, state: "empty"};
+          this.letters[i] = {text: '', state: 'empty'};
         }
       },
       toggleState(index) {
-        let states = ["empty", "absent", "present", "correct"];
         switch(this.letters[index].state) {
-          case "empty": this.letters[index].state = "absent"; break;
-          case "absent": this.letters[index].state = "present"; break;
-          case "present": this.letters[index].state = "correct"; break;
-          case "correct": this.letters[index].state = "empty";
+          case 'absent': this.letters[index].state = 'present'; break;
+          case 'present': this.letters[index].state = 'correct'; break;
+          case 'correct': this.letters[index].state = 'absent';
         }
+      },
+      handleInput($event) {
+        let letters = /^[A-Za-z]+$/;
+        if ($event.key.match(letters) && $event.key.length === 1) {
+          let letter = $event.key.toUpperCase();
+          this.addLetterToGrid(letter);
+        } else if ($event.keyCode === 8 || $event.keyCode === 46) {
+          this.deleteLetterFromGrid()
+        } else {
+          $event.preventDefault();
+        }
+        this.updateWordList();
+      },
+      addLetterToGrid(letter) {
+        if (this.indexCounter < 30) {
+          this.letters[this.indexCounter].text = letter;
+          this.letters[this.indexCounter].state = 'absent';
+          this.indexCounter += 1;
+        }
+      },
+      deleteLetterFromGrid() {
+        if (this.indexCounter > 0) {
+          this.indexCounter -= 1;
+          this.letters[this.indexCounter].text = '';
+          this.letters[this.indexCounter].state = 'empty';
+        }
+      },
+      updateWordList() {
+        console.log('Updated')
       }
     }
   }).mount('#app')
